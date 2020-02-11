a99b1cb737a381076f70d0bc0b69959557a9e2b9
Andrii Piatakha <andrey.pyatakha@gmail.com>
2020-02-12 00:16:29 +0200

    Comments and Java Doc lesson

---
+++ src/com/itbulls/learnit/javacore/javadoc/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.itbulls.learnit.javacore.javadoc;
+
+/**
+ * 
+ * Hello World Application
+ * 
+ * @author Andrii_Piatakha
+ *
+ */
+public class HelloWorld {
+	
+	/**
+	 * The entry point to the program.
+	 * 
+	 * @param args input arguments
+	 */
+	public static void main(String[] args) {
+		/*
+		 * Please, do not remove
+		 * this code.
+		 */
+		
+		System.out.println("Hello World");
+		
+		// TODO add more code here in the future
+	}
+}

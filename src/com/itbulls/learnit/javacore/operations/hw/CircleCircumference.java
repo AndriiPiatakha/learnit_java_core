82e9c16284a0691afd5c5b5fd6664c87c73dca81
Andrii Piatakha <andrey.pyatakha@gmail.com>
2020-03-23 00:12:56 +0200

    Circle Circumference

---
+++ src/com/itbulls/learnit/javacore/operations/hw/CircleCircumference.java
@@ -0,0 +1,18 @@
+package com.itbulls.learnit.javacore.operations.hw;
+
+import java.util.Scanner;
+
+public class CircleCircumference {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Please, enter circle radius: ");
+		double radius = sc.nextDouble();
+		
+		double circleCircumference = 2 * Math.PI * radius;
+		
+		System.out.println("Circle circumference is: " + circleCircumference);
+	}
+
+}
---
+++ test/com/itbulls/learnit/javacore/operations/hw/CircleCircumferenceTest.java
@@ -0,0 +1,55 @@
+package com.itbulls.learnit.javacore.operations.hw;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CircleCircumferenceTest {
+	
+	private static final String RESULT_SEPARATOR = "Circle circumference is: ";
+
+	private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+	private final PrintStream originalOut = System.out;
+
+	@Before
+	public void setStreams() {
+		System.setOut(new PrintStream(out));
+	}
+
+	@After
+	public void restoreInitialStreams() {
+		System.setOut(originalOut);
+	}
+
+	@Test
+	public void shouldCalculateTriangleArea1() {
+		String input = "10";
+		InputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		CircleCircumference.main(new String[] {});
+
+		String[] result = out.toString().trim().split(RESULT_SEPARATOR);
+		assertEquals(62.83185307179586, Double.parseDouble(result[1]), 0.01);
+	}
+
+	@Test
+	public void shouldCalculateTriangleArea2() {
+		String input = "5";
+		InputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		CircleCircumference.main(new String[] {});
+
+		String[] result = out.toString().trim().split(RESULT_SEPARATOR);
+		assertEquals(31.41592653589793, Double.parseDouble(result[1]), 0.01);
+	}
+
+}

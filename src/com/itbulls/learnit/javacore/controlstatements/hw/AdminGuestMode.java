3accdb234c97b165c02eccda478bc0c3e02e9e99
Andrii Piatakha <andrey.pyatakha@gmail.com>
2020-04-14 20:14:26 +0300

    Removed constants from AdminGuestMode
    Using local variables instead. Students at this point are not aware
    about constants. So for this exercise let's keep using local variables.

--- src/com/itbulls/learnit/javacore/controlstatements/hw/AdminGuestMode.java
+++ src/com/itbulls/learnit/javacore/controlstatements/hw/AdminGuestMode.java
@@ -2,17 +2,16 @@ package com.itbulls.learnit.javacore.controlstatements.hw;
 
 public class AdminGuestMode {
 	
-	public static final String ADMIN_MODE = "--admin";
-	public static final String GUEST_MODE = "--guest";
-	
 	public static void main(String[] args) {
 		String inputArguments = String.join(",", args);
-		if (inputArguments.contains(ADMIN_MODE) && inputArguments.contains(GUEST_MODE)) {
+		String adminFlag = "--admin";
+		String guestFlag = "--guest";
+		if (inputArguments.contains(adminFlag) && inputArguments.contains(guestFlag)) {
 			System.out.println("Please, select either 'ADMIN' or "
 					+ "'GUEST' mode for this program");
-		} else if (inputArguments.contains(ADMIN_MODE)) {
+		} else if (inputArguments.contains(adminFlag)) {
 			System.out.println("Hello, Admin!");
-		} else if (inputArguments.contains(GUEST_MODE)) {
+		} else if (inputArguments.contains(guestFlag)) {
 			System.out.println("Hello, Guest!");
 		}
 		

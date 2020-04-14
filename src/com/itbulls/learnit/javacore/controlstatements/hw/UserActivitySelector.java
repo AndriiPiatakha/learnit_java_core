ed5cd3b23ca94297c0d2023fb463a631abad1348
Andrii Piatakha <andrey.pyatakha@gmail.com>
2020-04-14 20:28:42 +0300

    Refactoring: formatting for switch statement

--- src/com/itbulls/learnit/javacore/controlstatements/hw/UserActivitySelector.java
+++ src/com/itbulls/learnit/javacore/controlstatements/hw/UserActivitySelector.java
@@ -10,26 +10,26 @@ public class UserActivitySelector {
 		String userInput = sc.nextLine();
 
 		switch (userInput) {
-		case "login":
-			System.out.println("Please, enter your username");
-			break;
-		case "sign_up":
-			System.out.println("Welcome!");
-			break;
-		case "terminate_program":
-			System.out.println("Thank you! Good bye!");
-			break;
-		case "main_menu":
-			System.out.println("Main menu");
-			break;
-		case "about_app":
-			System.out.println("This app is created by me with "
-					+ "support of \u00AEIT-Bulls.com");
-			break;
-		default:
-			System.out.println("Please, enter one of these values: login, "
-					+ "sign_up, terminate_program, main_menu, about_app");
-			break;
+			case "login":
+				System.out.println("Please, enter your username");
+				break;
+			case "sign_up":
+				System.out.println("Welcome!");
+				break;
+			case "terminate_program":
+				System.out.println("Thank you! Good bye!");
+				break;
+			case "main_menu":
+				System.out.println("Main menu");
+				break;
+			case "about_app":
+				System.out.println("This app is created by me with "
+						+ "support of \u00AEIT-Bulls.com");
+				break;
+			default:
+				System.out.println("Please, enter one of these values: login, "
+						+ "sign_up, terminate_program, main_menu, about_app");
+				break;
 		}
 
 	}

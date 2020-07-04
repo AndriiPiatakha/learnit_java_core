b76fdec537a3a15a5a59fdf933762a006fd3a4b3
Andrii Piatakha <andrey.pyatakha@gmail.com>
2020-07-04 22:02:53 +0300

    Class lesson demo

--- src/com/itbulls/learnit/javacore/oop/classes/Demo.java
+++ src/com/itbulls/learnit/javacore/oop/classes/Demo.java
@@ -13,20 +13,20 @@ public class Demo {
 		
 		Cart cart = new Cart();
 		System.out.println(cart);
-//		
-//		System.out.println();
-//		System.out.println("========== CUSTOM CONSTRUCTOR DEMO ==========");
-//		
-//		Cart cart2 = new Cart(2, 2);
-//		System.out.println(cart2);
-//		
-//		System.out.println();
-//		System.out.println("========== ADD PRODUCTS TO CART ==========");
-//		
-//		cart.addProduct(new Product("L shaped Showerbath", BigDecimal.valueOf(300)));
-//		cart.addProduct(new Product("P Shaped Right Hand Bath", BigDecimal.valueOf(200)));
-//
-//		System.out.println(cart);
+		
+		System.out.println();
+		System.out.println("========== CUSTOM CONSTRUCTOR DEMO ==========");
+		
+		Cart cart2 = new Cart(2, 2);
+		System.out.println(cart2);
+		
+		System.out.println();
+		System.out.println("========== ADD PRODUCTS TO CART ==========");
+		
+		cart.addProduct(new Product("L shaped Showerbath", BigDecimal.valueOf(300)));
+		cart.addProduct(new Product("P Shaped Right Hand Bath", BigDecimal.valueOf(200)));
+
+		System.out.println(cart);
 		
 	}
 }

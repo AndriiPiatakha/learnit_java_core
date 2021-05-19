815f6b9825e8f8a6d3fe81b2b085707576a955a0
Andrey Pyatakha <andrey.pyatakha@gmail.com>
2021-05-19 17:59:01 +0300

    Composite pattern example

---
+++ src/com/itbulls/learnit/javacore/patterns/structural/composite/Shape.java
@@ -0,0 +1,16 @@
+package com.itbulls.learnit.javacore.patterns.structural.composite;
+
+import java.awt.*;
+
+public interface Shape {
+    public int getX();
+    public int getY();
+    public int getWidth();
+    public int getHeight();
+    public void move(int x, int y);
+    public Boolean isInsideBounds(int x, int y);
+    public void select();
+    public void unSelect();
+    public Boolean isSelected();
+    public void paint(Graphics graphics);
+}
